a5d5736260b31aab604bb330126173a87713ae84
Ashish Kumar <akum0988@gmail.com>
2025-05-08 17:54:41 +0530

    Add files via upload
    Task 1: Check if a Number is Even or Odd
    Task 2: Sum of Integers from 1 to 50 Using a Loop

---
+++ Assignment_2.py
@@ -0,0 +1,19 @@
+#Task 1: Check if a Number is Even or Odd
+
+n= int(input('enter a number: '))
+if n % 2 == 0:
+    print(n, 'is an even number')
+else:
+     print(n, 'is an odd number')
+
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+n = 0
+for i in range(1, 51):
+    n += i
+
+print("The sum of numbers from 1 to 50 is:", n)
+
+
+
+
+
